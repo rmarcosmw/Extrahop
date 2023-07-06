29d8d267da86491dcf8d9997c2761b2fe4474f9a
Marcos <r.marcos.mw@protonmail.com>
2023-07-07 01:32:21 +0700

    Create DBLogin.js
    this trigger is to track and records DB user activity and send it to recordstore on Extrahop

---
+++ DBLogin.js
@@ -0,0 +1,13 @@
+#EVENT trigger DB_RESPONSE Assignment as needed
+
+var dbuser = ['root','sa','sys','system'];
+var i;
+for (i=0; i < dbuser.length; i++) {
+  switch(dbuser) {
+	case dbuser[i];
+		//DB.commitRecord();
+		debug('dbuser: ' + dbuser[i] + '/n'
+			'db length: ' + dbuser length);
+		break;
+	}
+}
